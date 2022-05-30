db48535fbc012a09e06423e3bdfffa5806845cf2
markoclimako <mkclimako@gmail.com>
2022-05-30 17:31:09 -0300

    feature|reset controls|done

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,22 +1,17 @@
-export default function Controls({
-    playBtn,
-    pauseBtn,
-  }) {
-    const handleTimerStatus  = () => {
-      playBtn.classList.toggle('hide');
-      pauseBtn.classList.toggle('hide');
-      pauseBtn.classList.toggle('selected');
+export default function Controls({ playBtn, pauseBtn }) {
+  const handleTimerStatus = () => {
+    playBtn.classList.toggle('hide');
+    pauseBtn.classList.toggle('hide');
+    pauseBtn.classList.toggle('selected');
+  };
 
-    };
-  
-    const resetControls = () => {
-        playBtn.classList.remove('hide');
-        pauseBtn.classList.add('hide')
-    };
-  
-    return {
-        handleTimerStatus,
-      resetControls,
-    };
-  }
-  
+  const resetControls = () => {
+    playBtn.classList.remove('hide');
+    pauseBtn.classList.add('hide');
+  };
+
+  return {
+    handleTimerStatus,
+    resetControls,
+  };
+}
