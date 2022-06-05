92ebfeaae965df5a648f213aee45034a263615e2
markoclimako <mkclimako@gmail.com>
2022-06-05 04:28:18 -0300

    fix|dark mode color changes

--- scripts/switchThemes.js
+++ scripts/switchThemes.js
@@ -1,10 +1,5 @@
 const SwitchTheme = ({ body, switchBtn }) => {
   body.classList.toggle('dark');
-  if (body.classList.contains('dark')) {
-    switchBtn.style.transform = 'matrix(-1, 0, 0, 1, 0, 0)';
-  } else {
-    switchBtn.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
-  }
 };
 
 export default SwitchTheme;
